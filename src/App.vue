e3cfba5fc9c28cb9aac9e5ad4514ceccf7c3845d
BznGit <bez-al@mail.ru>
2023-11-03 12:29:32 +0300

    tune dependencie

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>Double count is {{ userStore.double }}</p>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { useCounterStore } from './store/index'
+
+
 
 export default {
+  setup() {
+    const userStore = useCounterStore();
+    return { userStore };
+  },
   name: 'App',
   components: {
     HelloWorld
