99920a8d7681cc61bab41c27300ad0189c822527
BznGit <bez-al@mail.ru>
2023-11-07 11:35:04 +0300

    dp

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createPinia } from 'pinia'
-import router from './router'
-
-createApp(App).use(router).use(createPinia()).mount('#app')
